e42ddc433cf09bb5dbf45acd39bd3c1cc731c5f6
LukeBeaumont <lbeaumont50@msn.com>
2023-01-14 18:52:07 +0000

    Complete reverseString exercise and pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) { 
+    let newArray = string.split("");
 
+    let reverseArray = newArray.reverse();
+
+    let joinArray = reverseArray.join("");
+
+    return joinArray;
 };
 
 // Do not edit below this line
