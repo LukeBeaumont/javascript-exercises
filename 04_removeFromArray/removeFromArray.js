516571293d62ff54dcdfabae304955362b175a82
LukeBeaumont <lbeaumont50@msn.com>
2023-01-16 20:07:23 +0000

    Complete exercise removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    var array = args [0];
+    var newArray = [];
 
-};
-
+    array.forEach((item) => { 
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }})
+        return newArray;
+    };
 // Do not edit below this line
 module.exports = removeFromArray;
